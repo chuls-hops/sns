486effdfa4e8b235bf466c796d028959421406c4
chulshps <chulshops@gmail.com>
2023-02-03 14:40:25 +0900

    20230203

---
+++ src/main/java/com/fastcampus/sns/controller/PostController.java
@@ -0,0 +1,29 @@
+package com.fastcampus.sns.controller;
+
+import com.fastcampus.sns.configureration.AuthenticationConfig;
+import com.fastcampus.sns.controller.request.PostCreateRequest;
+import com.fastcampus.sns.controller.response.Response;
+import com.fastcampus.sns.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/posts")
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping
+    public Response<Void> create(@RequestBody PostCreateRequest request, Authentication authentication) {
+        postService.create(request.getTitle(), request.getBody(), authentication.getName());
+        return Response.success();
+
+    }
+
+}
